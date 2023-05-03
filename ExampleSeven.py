f6edb33eca550d9b1ef538213628dd165a003630
DemonMaike <dmitry.malyshevv@ya.ru>
2023-05-03 13:42:04 +0400

    Apply ExampleSeven

---
+++ ExampleSeven.py
@@ -0,0 +1,63 @@
+# более менее реальная задача
+
+import requests
+from time import time
+# Синхронный стиль
+
+def get_file(url):
+    response = requests.get(url, allow_redirects=True)
+    return response
+
+def write_file(response):
+    filename = response.url.split('/')[-1]
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+        
+def main():
+    start_time = time()
+    
+    url = "https://loremflickr.com/320/240"
+    
+    for i in range(10):
+        write_file(get_file(url))
+        
+    end_time = time()
+    
+    print(end_time - start_time)
+    
+if __name__ == '__main__':
+    main()
+    
+# ----------------------------------------------------------
+# Асинхронный стиль
+# для работы с http устанавливаем pip install aiohttp
+import aiohttp
+import asyncio
+
+def write_image(data):
+    filename = f'file-{int(time() * 1000)}.jpeg'
+    
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+async def fetch_content(url, session):
+    async with session.get(url, allow_redirects=True) as response:
+        data = await response.read()
+        write_image(data)
+        
+async def main2():
+    url = "https://loremflickr.com/320/240"
+    tasks = []
+    
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            task = asyncio.create_task(fetch_content(url, session))
+            tasks.append(task)
+            
+        await asyncio.gather(*tasks)
+        
+if __name__ == '__main__':
+    t0 = time()
+    asyncio.run(main2())
+    t1 = time()
+    print(t1-t0)
--- ExampleSix.py
+++ ExampleSix.py
@@ -4,31 +4,40 @@
 import asyncio
 
 
-@asyncio.coroutine
-def print_nums():
+#@asyncio.coroutine
+async def print_nums():
     num = 1
     while 1:
         print(num)
         num += 1
-        yield from asyncio.sleep(1) # Слип работает как блокирующая функция, по этому нужно использовать ее asyinc весрию 
- 
-@asyncio.coroutine
-def print_time():
+        #yield from asyncio.sleep(1) # Слип работает как блокирующая функция, по этому нужно использовать ее asyinc весрию 
+        await asyncio.sleep(0.2)
+        
+#@asyncio.coroutine
+async def print_time():
     count = 0
     while 1:
         if count % 3 == 0:
             print(f'{count} seconds have passed')
         count += 1
-        yield from asyncio.sleep(1)
-
-@asyncio.coroutine 
-def main():
-    task_1 = asyncio.ensure_future(print_nums())
-    task_2 = asyncio.ensure_future(print_time())
+        #yield from asyncio.sleep(1)
+        await asyncio.sleep(0.5)
+#@asyncio.coroutine 
+async def main():
+    #task_1 = asyncio.ensure_future(print_nums())
+    #task_2 = asyncio.ensure_future(print_time())
+    task_1 = asyncio.create_task(print_nums())
+    task_2 = asyncio.create_task(print_time())
     
-    yield from asyncio.gather(task_1, task_2)
- 
+    #yield from asyncio.gather(task_1, task_2)
+    await asyncio.gather(task_1,task_2)
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+    asyncio.run(main())
+    
+
+# После Python -v 3.5  появились новые термины, @asyncio.corutine = async def, yield from = await
+# После Python -v 3.6 ensure_future = create_task
+# После Python -v 3.7 замена конструкции с loop первратилась в asincio run
