b1efeaaa140338d1b2f9a960e019895cfb42c8a6
DemonMaike <dmitry.malyshevv@ya.ru>
2023-05-09 01:03:05 +0400

    Added example about threads

---
+++ Course/treads.py
@@ -0,0 +1,17 @@
+# This code preview the treading in python
+# tread in this code is real tread in OS and it can look if to use command 'ps -T'
+
+from threading import Thread
+from time import sleep, time
+
+def tread_func():
+    sleep(100)
+    
+tread_1 = Thread(target=tread_func)
+tread_2 = Thread(target=tread_func)
+
+if __name__ == '__main__':
+    tread_1.start()
+    tread_2.start()
+    tread_1.join()
+    tread_2.join()
